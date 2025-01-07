9943486128aff64ecefda297bb9e9d9892d116ab
unknown <sagargondaliya50@gmail.com>
2025-01-07 17:32:50 +0000

    hiii

---
+++ git_push.bat
@@ -0,0 +1,18 @@
+@echo off
+REM Change directory to the repository (optional)
+cd /d "%~dp0"
+
+REM Stage all changes
+git add .
+
+REM Commit changes with a default message
+echo Enter commit message:
+set /p commitMsg=
+git commit -m "%commitMsg%"
+
+REM Push changes to the remote repository
+git push origin master
+
+REM Confirmation message
+echo Changes pushed to GitHub successfully!
+pause
